1beb559fd4d557cd168bfdfb6d203d43e19b7f4a
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-04 21:52:14 -0800

    some tests

--- AfferentDemosTests/Main.lean
+++ AfferentDemosTests/Main.lean
@@ -3,19 +3,8 @@
 -/
 import Crucible
 import Demos.Runner
-
-namespace AfferentDemosTests
-
-open Crucible
-
-testSuite "AfferentDemos"
-
-test "smoke" := do
-  1 ≡ 1
-
-#generate_tests
-
-end AfferentDemosTests
+import AfferentDemosTests.Smoke
+import AfferentDemosTests.StrokeCards
 
 def main : IO UInt32 := do
   runAllSuites
